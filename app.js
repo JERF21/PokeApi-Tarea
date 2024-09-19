4e7ea82301896371daaebe560a65c0b843d4231c
JERF21 <eduardo21reyes@gmail.com>
2024-09-19 18:01:28 -0400

    Mejora en los detalles y seleccion de tipos

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const API_URL = "https://pokeapi.co/api/v2";
 let currentPage = 1;
 let pokemonType = "";
 let totalPages = 1;
+let selectedTypes = [];
+
+const traduccionesTipos = {
+fire: "Fuego", water: "Agua", grass: "Planta", electric: "Eléctrico", ice: "Hielo", fighting: "Lucha", poison: "Veneno", ground: "Tierra",
+flying: "Volador", psychic: "Psíquico", bug: "Bicho", rock: "Roca", ghost: "Fantasma",  dragon: "Dragón",  dark: "Siniestro",  steel: "Acero",
+fairy: "Hada", normal: "Normal",
+}
+
 
 /**Buscar por nombre */
 function buscarPokemon() {
@@ -14,29 +22,51 @@ function buscarPokemon() {
   }
 }
 
-/**para las paginas */
-function tipoPokemon(type, page = 1) {
+/**para seleccionar hasta 2 tipos */
+function seleccionarTipo(type, button) {
+  if (selectedTypes.includes(type)) {
+    selectedTypes = selectedTypes.filter(t => t !== type);
+    button.classList.remove("selected");
+  } else if (selectedTypes.length < 2) {
+    selectedTypes.push(type);
+    button.classList.add("selected");
+  }
+
+  if (selectedTypes.length > 0) {
+    buscarPokemonPorTipos(selectedTypes);
+  } else {
+    alert("Selecciona al menos un tipo.");
+  }
+}
+
+/**Buscar Pokémon por tipos */
+function buscarPokemonPorTipos(types, page = 1) {
   currentPage = page;
-  pokemonType = type;
 
-  fetch(`${API_URL}/type/${type}`)
-    .then((response) => response.json())
-    .then((data) => {
-      const startIndex = (page - 1) * 20;
-      const endIndex = startIndex + 20;
-      const paginatedPokemon = data.pokemon.slice(startIndex, endIndex);
+  const typePromises = types.map(type => fetch(`${API_URL}/type/${type}`).then(res => res.json()));
 
-      totalPages = Math.ceil(data.pokemon.length / 20);
+  Promise.all(typePromises).then(data => {
+    let pokemonList = data[0].pokemon.map(p => p.pokemon);
 
-      const promises = paginatedPokemon.map((p) =>
-        fetch(p.pokemon.url).then((res) => res.json())
+    if (types.length === 2) {
+      const secondTypePokemon = data[1].pokemon.map(p => p.pokemon);
+      pokemonList = pokemonList.filter(pokemon =>
+        secondTypePokemon.some(secondTypeP => secondTypeP.name === pokemon.name)
       );
-      Promise.all(promises).then((pokemonList) => {
-        mostrarPokemon(pokemonList);
-        actPaginacion();
-      });
-    })
-    .catch((error) => console.error(error));
+    }
+
+    const startIndex = (page - 1) * 20;
+    const endIndex = startIndex + 20;
+    const paginatedPokemon = pokemonList.slice(startIndex, endIndex);
+
+    totalPages = Math.ceil(pokemonList.length / 20);
+
+    const promises = paginatedPokemon.map(p => fetch(p.url).then(res => res.json()));
+    Promise.all(promises).then(pokemonData => {
+      mostrarPokemon(pokemonData);
+      actPaginacion();
+    });
+  }).catch(error => console.error(error));
 }
 
 /**mostrar en la pagina */
@@ -89,10 +119,15 @@ function actPaginacion() {
 
 /**mostrar detalles del pokemon */
 function detallePokemon(pokemon) {
+  const modal = document.getElementById("pokemonModal");
   const detailsDiv = document.getElementById("pokemonDetails");
+  const tipos = pokemon.types.map(type => traduccionesTipos[type.type.name] || type.type.name).join(", ");
+  const sprite = pokemon.sprites.versions['generation-v']['black-white'].animated.front_default || pokemon.sprites.front_default;
+
   detailsDiv.innerHTML = `
     <h2>${pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</h2>
-    <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}">
+    <img src="${sprite}" alt="${pokemon.name}">
+    <p><strong>Tipos:</strong> ${tipos}</p>
     <p><strong>Altura:</strong> ${pokemon.height / 10} m</p>
     <p><strong>Peso:</strong> ${pokemon.weight / 10} kg</p>
     <p><strong>Habilidades:</strong> ${pokemon.abilities.map(ability => ability.ability.name).join(", ")}</p>
@@ -101,5 +136,17 @@ function detallePokemon(pokemon) {
       ${pokemon.stats.map(stat => `<li>${stat.stat.name}: ${stat.base_stat}</li>`).join("")}
     </ul>
   `;
-  detailsDiv.style.display = "block";
+  modal.style.display = "block";
+}
+
+function cerrarModal() {
+  const modal = document.getElementById("pokemonModal");
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  const modal = document.getElementById("pokemonModal");
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
